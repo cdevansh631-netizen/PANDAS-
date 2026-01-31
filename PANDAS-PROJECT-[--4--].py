6196b34b5ff9d1809d416973ef534bc95727a62c
Devansh Chauhan <cdevansh631@gmail.com>
2026-02-01 00:16:26 +0530

    CHANGES

--- PANDAS-PROJECT-[--4--].py
+++ PANDAS-PROJECT-[--4--].py
@@ -51,7 +51,7 @@ print(df.tail(3),"\n")
 
 #CHECKING OF SHAPE,COLUMN AND DATATYPE:-
 print(df.info(),"\n")
-print("(ROW,COLUMN):",df.shape."\n")
+print("(ROW,COLUMN):",df.shape,"\n")
 
 #MISSING VALUES:-
 print(df.isnull().sum(),"\n")
@@ -60,4 +60,71 @@ print(df.isnull().sum(),"\n")
 df.drop_duplicates("Order_ID",inplace=True)
 
 #REPLACE NAN VALUE IN DISCOUNT WITH 0:-
-df.replace(np.nan,"0",inplace=True)
+df.replace(np.nan,0,inplace=True)
+
+#DISCOUNT FLAG:-
+df.loc[df["Discount"]>0,"Discount_Flag"]="YES"
+df.loc[df["Discount"]==0,"Discount_Flag"]="NO"
+
+#REVENUE COLUMN:-
+df["REVENUE"]=df["Units_Sold"]*df["Selling_Price"]
+#COST COLUMN:-
+df["COST"]=df["Units_Sold"]*df["Cost_Price"]
+#PROFIT_COLUMN:-
+df["PROFIT"]=df["REVENUE"]-df["COST"]
+#PROFIT_MARGIN COLUMN:-
+df["PROFIT_MARGIN"]=((df["PROFIT"]/df["COST"])*100).round(2)
+
+#TOTAL REVENUNE / TOTAL PROFIT :-
+print("TOTAL PROFIT :",df["PROFIT"].sum())
+print("TOTAL REVENUE :",df["REVENUE"].sum(),"\n")
+
+#PRODUCT WISE TOTAL PROFIT :-
+print("PRODUCT WISE TOTAL PROFIT :-")
+print(df.groupby("Product").agg("PROFIT").sum(),"\n")
+
+#REGION WISE AVERAGE PROFIT :
+print("REGION WISE AVERAGE PROFIT :-")
+print(df.groupby("Region").agg({"PROFIT":"mean"}),"\n")
+
+#TOP PROFIT PRODUCT:-
+print("TOP PROFIT PRODUCT :-")
+print(df.groupby("Product")["PROFIT"].sum().idxmax(),"\n")
+
+#LEAST PROFITABLE REGION:-
+print("LEAST PROFITABLE REGION :-")
+print(df.groupby("Region")["PROFIT"].sum().idxmin(),"\n")
+
+#ORDER WHERE PROFIT<20,000 :-
+print("ORDER WHERE PROFIT < 20,000 :-",)
+print(df.loc[(df["PROFIT"]<20000),"Order_ID"].to_string(index=False),"\n")
+
+#ORDER WHERE PROFIT _MARGIN > 30 :-
+print("ORDER WHERE PROFIT MARGIN >30 :-")
+print(df.loc[df["PROFIT_MARGIN"]>30,"Order_ID"].to_string(index=False),"\n")
+
+#SORTING BY PROFIT :-
+print(df[df["PROFIT"]<20000].sort_values("PROFIT",ascending=False))
+
+#EXTRACT YEAR AND MONTH:-
+df["Order_Date"]=pd.to_datetime(df["Order_Date"])
+df["YEAR"]=df["Order_Date"].dt.year
+df["Month"]=df["Order_Date"].dt.month
+df["Month_Name"]=df["Order_Date"].dt.month_name()
+print(df[["Order_Date","YEAR","Month","Month_Name"]],"\n")
+
+#MONTH WISE TOTAL SALE:-
+print("MONTH WISE TOTAL SALE :-")
+print(df.groupby(df["Month"]).agg({"REVENUE":"sum"}),"\n")
+
+#BEST MONTH PROFIT :-
+print("BEST MONTH PROFIT :-")
+print(df.groupby(df["Month"])["PROFIT"].sum().idxmax(),"\n")
+
+#CUSTOMER WISE TOTAL PROFIT :-
+print("CUSTOMER WISE TOTAL PROFIT :-")
+print(df.groupby("Customer").agg({"PROFIT":"sum"}),"\n")
+
+#STAR PRODUCT OF NORTH REGION:-
+print("STAR PRODUCT OF NORTH REGION :-")
+print(df.loc[])
