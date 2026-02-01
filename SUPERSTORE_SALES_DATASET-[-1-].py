96691f8d30464c77562d37ef4062c92e2aa11b05
Devansh Chauhan <cdevansh631@gmail.com>
2026-02-01 22:37:34 +0530

    C

--- SUPERSTORE_SALES_DATASET-[-1-].py
+++ SUPERSTORE_SALES_DATASET-[-1-].py
@@ -1,7 +1,23 @@
 #1ST KAGGLE DATASET.-[--SUPERSTORE_SALES_DATASET--] :-
+import pandas as pd
+ 
+df=pd.read_csv("train.csv")
+print(df.columns)
+
 
 import pandas as pd 
 df=pd.read_csv("train.csv")
-# print(df.columns)
-# print(df.head().T)
-print(df.iloc[:,0:3])
+print(df.columns)
+
+#UNDERSTANDING DATA:-
+print("NDERSTANDING DATA:-","\n")
+print("COLUMNS :-",df.columns,"\n")
+print("DATA TYPE :-",df.info(),"\n")
+print(df.describe(include="object"))
+print("(ROWS,COLUMN) :-",df.shape,"\n")
+
+
+# #DATA HANDLING :-
+# print(df.isnull().sum())
+# # print(df.drop_duplicates())
+# print(df.isnull.sum())
